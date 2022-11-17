2aea2c2e03945f9e048a99b2bfb9a61f96d6cf22
mangesSh <bhaganagaremangesh@gmail.com>
2022-11-17 19:00:27 +0530

    changes reverted from the code

--- src/FromWrapper.tsx
+++ src/FromWrapper.tsx
@@ -8,7 +8,6 @@ type FormWrapperProps={
 export function FormWrapper({ title, children}:FormWrapperProps){
 return <>
 <h2 style={{ textAlign:"center", margin:0, marginBottom:"2rem" }}>{title}</h2>
-<h3 style={{ textAlign:"center", margin:0, marginBottom:"2rem" }}>{title}</h3>
 <div style={{ display: "grid", gap:" 1rem .5rem", justifyContent: "Flex-start", gridTemplateColumns:"auto minmax(auto, 400px)"}}>{children}</div>
 </>
 }
